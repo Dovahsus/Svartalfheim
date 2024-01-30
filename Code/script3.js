b7e1629423462176591dba63063dea43922748a3
Dovahsus <ym.obata@hotmail.com>
2024-01-30 14:04:44 +0100

    Vanaheim

--- Code/script3.js
+++ Code/script3.js
@@ -271,4 +271,10 @@
 //         console.log(`${brian.fullName}'s BMI (${brian.bmi}) is higher than ${mike.fullName}'s BMI (${mike.bmi})`)
 // }
 
+// console.log('Lifting weights repetition 1');
 
+// for loop keeps running while condition is true
+for (let rep = 1; rep <= 100; rep++){
+    console.log(`Lifting weights repetition ${rep}`);
+
+}
