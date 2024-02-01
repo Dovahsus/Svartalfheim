3e61dc31a0bb19cc26281d38c18ec7094679e22f
Dovahsus <ym.obata@hotmail.com>
2024-02-01 10:01:13 +0100

    Vanaheim
    Vanaheim

--- Code/script3.js
+++ Code/script3.js
@@ -336,16 +336,16 @@
 //   console.log(`Lifting weights rep ${rep}`);
 // }
 
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`Lifting weights rep ${rep}`);
-  rep++;
-}
+// let rep = 1;
+// while (rep <= 10) {
+//   // console.log(`Lifting weights rep ${rep}`);
+//   rep++;
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+// let dice = Math.trunc(Math.random() * 6) + 1;
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log(`6...loop is about to end...`);
-}
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log(`6...loop is about to end...`);
+// }
