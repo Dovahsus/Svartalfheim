63748557417b1af60b5bda0b329afcdc67f6e371
Dovahsus <ym.obata@hotmail.com>
2024-02-07 09:27:26 +0100

    second

--- Code/GuessNumber/Guess.js
+++ Code/GuessNumber/Guess.js
@@ -11,19 +11,26 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber;
-
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // Input bound
   if (!guess) {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
-  } else if (guess > secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('body').style.width = '30rem';
+  }
+
+  // Guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number is too high';
       score--;
@@ -32,12 +39,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lose!!!';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  // Guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Guess is too low';
       score--;
       document.querySelector('.score').textContent = score;
-    } else {
+    }
+    // Player loses
+    else {
       document.querySelector('.message').textContent = 'You lose!!!';
       document.querySelector('.score').textContent = 0;
     }
