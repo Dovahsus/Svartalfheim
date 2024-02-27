93015467578990378da7f098b71a8710a67a0da0
Dovahsus <ym.obata@hotmail.com>
2024-02-28 00:21:12 +0100

    Midguard

---
+++ Code/Lamborghini/Script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const automobili = {
+  name: 'Lamborghini',
+  location: 'Sant’Agata Bolognese, Italy',
+  platforms: ['V12', 'V10', 'V8', 'Hybrid'],
+  squadraCorsa: ['GT3 Gallardo', 'Huracan Super Trofeo Evo 2', 'Diablo G-TT'],
+  flagShip: ['Revulto', 'Huracan', 'Urus,'],
+};
