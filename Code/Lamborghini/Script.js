69df3221751f17ab9495714b93807d512e47a7cd
Dovahsus <ym.obata@hotmail.com>
2024-03-21 12:53:43 +0100

    Joutonheim

--- Code/Lamborghini/Script.js
+++ Code/Lamborghini/Script.js
@@ -40,8 +40,12 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1} ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
-
+// 1) Destructuring
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 
@@ -55,6 +59,24 @@ const [pizza, , risotto, ...otherFood] = [
 ];
 console.log(pizza, risotto, otherFood);
 
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(i);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza();
 // /////////////////////////////////////////////////////////////
 // // The Spread Operator (...)
 // const arr = [7, 8, 9];
